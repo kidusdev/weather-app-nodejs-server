637df0598942e98b92f4d96bcfa4914f5ad0497e
Kidus Abrham <156700000+kidusdev@users.noreply.github.com>
2024-06-18 16:01:04 +0300

    Create sales.js

---
+++ routes/sales.js
@@ -0,0 +1,16 @@
+const sales = Array(10000).fill(0).map((_,i) => ({
+    "id": i,
+    "admin_name": "female",
+    "item_name": "Senior",
+    "unit_price": 58190.84,
+    "total_price": 12907.66,
+    "unit_profit": 73710.11,
+    "total_profit": 20082.52,
+    "admin_id": 96752,
+    "item_id": 53100,
+    "org_id": 61900,
+    "store_id": 28099,
+    "createdAt": "2024-06-17T13:33:01.752Z"
+}));
+
+export default sales;
