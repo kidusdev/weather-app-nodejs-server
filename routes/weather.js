0a0e51d6b605edf08cef6cb591df8aba55c53754
Kidus Abrham <156700000+kidusdev@users.noreply.github.com>
2024-06-18 16:02:02 +0300

    Update weather.js

--- routes/weather.js
+++ routes/weather.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var sales = require('sales.js');
 var router = express.Router();
 
 router.get('/:location?', function (req, res, next) {
   res.json(getStubWeatherData(req.params.location));
+});
+
+router.get('/json', function (req, res, next) {
+  res.json(sales);
 });
 
 function getStubWeatherData(location) {
